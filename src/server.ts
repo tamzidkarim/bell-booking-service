36485fcc5f9656ca5ab1181855a788db315c66c3
tamzidkarim <e.k.tamzid@gmail.com>
2023-01-16 09:17:36 -0500

    project initiation

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import '@/index';
+import App from '@/app';
+import IndexRoute from '@routes/index.route';
+import validateEnv from '@utils/validateEnv';
+
+validateEnv();
+
+const app = new App([new IndexRoute()]);
+
+app.listen();
